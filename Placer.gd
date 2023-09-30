3dc8e5e55c6a22b2c584cd28427fc26031d78888
halfpenny <ZMAC604@live.rhul.ac.uk>
2023-09-30 14:15:37 +0100

    Tower Placing (kinda)

---
+++ Placer.gd
@@ -0,0 +1,30 @@
+extends Sprite2D
+
+var pos = Vector2(0, 0)
+var snap = 128
+
+func place():
+	#var tower = get_node("Tower")
+	#var new = tower.duplicate()
+	#get_parent().add_child(new)
+	#new.position.x = pos.x
+	#new.position.y = pos.y
+	print(pos)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pos = get_global_mouse_position()
+	self.position.x = round(pos.x / snap) * snap
+	self.position.y = round(pos.y / snap) * snap
+#	print(pos)
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_P:
+			place()
+			print("TOWER DROPPED!!!!")
